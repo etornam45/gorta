0df937b92741021032b9c915f3f75bdcab51e241
Etornam <navidben45@gmail.com>
2026-05-28 12:43:57 +0000

    code format

--- password.go
+++ password.go
@@ -1,112 +1,112 @@
 package gorta
 
 import (
-    "crypto/rand"
-    "crypto/subtle"
-    "encoding/base64"
-    "errors"
-    "fmt"
-    "strings"
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"strings"
 
-    "golang.org/x/crypto/argon2"
+	"golang.org/x/crypto/argon2"
 )
 
 type argon2Params struct {
-    memory      uint32
-    iterations  uint32
-    parallelism uint8
-    saltLength  uint32
-    keyLength   uint32
+	memory      uint32
+	iterations  uint32
+	parallelism uint8
+	saltLength  uint32
+	keyLength   uint32
 }
 
 var defaultParams = argon2Params{
-    memory:      64 * 1024, // 64MB
-    iterations:  3,
-    parallelism: 2,
-    saltLength:  16,
-    keyLength:   32,
+	memory:      64 * 1024, // 64MB
+	iterations:  3,
+	parallelism: 2,
+	saltLength:  16,
+	keyLength:   32,
 }
 
 func hashPassword(password string) (string, error) {
-    salt := make([]byte, defaultParams.saltLength)
-    if _, err := rand.Read(salt); err != nil {
-        return "", fmt.Errorf("generating salt: %w", err)
-    }
+	salt := make([]byte, defaultParams.saltLength)
+	if _, err := rand.Read(salt); err != nil {
+		return "", fmt.Errorf("generating salt: %w", err)
+	}
 
-    hash := argon2.IDKey(
-        []byte(password),
-        salt,
-        defaultParams.iterations,
-        defaultParams.memory,
-        defaultParams.parallelism,
-        defaultParams.keyLength,
-    )
+	hash := argon2.IDKey(
+		[]byte(password),
+		salt,
+		defaultParams.iterations,
+		defaultParams.memory,
+		defaultParams.parallelism,
+		defaultParams.keyLength,
+	)
 
-    // Store as: $argon2id$v=19$m=65536,t=3,p=2$<salt>$<hash>
-    b64Salt := base64.RawStdEncoding.EncodeToString(salt)
-    b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+	// Store as: $argon2id$v=19$m=65536,t=3,p=2$<salt>$<hash>
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
 
-    encoded := fmt.Sprintf(
-        "$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s",
-        argon2.Version,
-        defaultParams.memory,
-        defaultParams.iterations,
-        defaultParams.parallelism,
-        b64Salt,
-        b64Hash,
-    )
+	encoded := fmt.Sprintf(
+		"$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s",
+		argon2.Version,
+		defaultParams.memory,
+		defaultParams.iterations,
+		defaultParams.parallelism,
+		b64Salt,
+		b64Hash,
+	)
 
-    return encoded, nil
+	return encoded, nil
 }
 
 func verifyPassword(password, encoded string) (bool, error) {
-    p, salt, hash, err := decodeHash(encoded)
-    if err != nil {
-        return false, err
-    }
+	p, salt, hash, err := decodeHash(encoded)
+	if err != nil {
+		return false, err
+	}
 
-    otherHash := argon2.IDKey(
-        []byte(password),
-        salt,
-        p.iterations,
-        p.memory,
-        p.parallelism,
-        p.keyLength,
-    )
+	otherHash := argon2.IDKey(
+		[]byte(password),
+		salt,
+		p.iterations,
+		p.memory,
+		p.parallelism,
+		p.keyLength,
+	)
 
-    if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
-        return true, nil
-    }
-    return false, nil
+	if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
+		return true, nil
+	}
+	return false, nil
 }
 
 func decodeHash(encoded string) (*argon2Params, []byte, []byte, error) {
-    parts := strings.Split(encoded, "$")
-    if len(parts) != 6 {
-        return nil, nil, nil, errors.New("invalid hash format")
-    }
+	parts := strings.Split(encoded, "$")
+	if len(parts) != 6 {
+		return nil, nil, nil, errors.New("invalid hash format")
+	}
 
-    var p argon2Params
-    _, err := fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d",
-        &p.memory, &p.iterations, &p.parallelism)
-    if err != nil {
-        return nil, nil, nil, fmt.Errorf("parsing params: %w", err)
-    }
+	var p argon2Params
+	_, err := fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d",
+		&p.memory, &p.iterations, &p.parallelism)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("parsing params: %w", err)
+	}
 
-    salt, err := base64.RawStdEncoding.DecodeString(parts[4])
-    if err != nil {
-        return nil, nil, nil, fmt.Errorf("decoding salt: %w", err)
-    }
+	salt, err := base64.RawStdEncoding.DecodeString(parts[4])
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("decoding salt: %w", err)
+	}
 
-    hash, err := base64.RawStdEncoding.DecodeString(parts[5])
-    if err != nil {
-        return nil, nil, nil, fmt.Errorf("decoding hash: %w", err)
-    }
+	hash, err := base64.RawStdEncoding.DecodeString(parts[5])
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("decoding hash: %w", err)
+	}
 
-    p.keyLength = uint32(len(hash))
-    p.saltLength = uint32(len(salt))
+	p.keyLength = uint32(len(hash))
+	p.saltLength = uint32(len(salt))
 
-    return &p, salt, hash, nil
+	return &p, salt, hash, nil
 }
 
 const dummyHash = "$argon2id$v=19$m=65536,t=3,p=2$c29tZXNhbHRzb21lc2FsdA$RMUMBkPmPJNqOmTDFOSOk6tfMaFJHhLbVPGCHDzJ8Ks"
